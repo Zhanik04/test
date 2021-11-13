0ca04b462b8fe39f5576b44d865e363e17a46050
Zhannur <zhannurkhan04@gmail.com>
2021-11-13 21:26:31 +0600

    dfdfsf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello world");
         System.out.println("Ya izuchayu github");
+        System.out.println("YErfsfs");
     }
 }
